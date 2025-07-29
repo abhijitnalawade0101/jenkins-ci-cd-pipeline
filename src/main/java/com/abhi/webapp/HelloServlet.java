4e4c7022144457d56ca0c789d2b1777ce5ecd15e
abhijitnalawade0101 <abhijitnalawade0101@gmail.com>
2025-07-29 17:28:29 +0000

    initial commit - maven web app with docker support

---
+++ src/main/java/com/abhi/webapp/HelloServlet.java
@@ -0,0 +1,14 @@
+package com.abhi.webapp;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Hello from Abhijeet’s WebApp!</h1>");
+    }
+}
+
