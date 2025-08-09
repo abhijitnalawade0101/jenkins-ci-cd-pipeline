b39457a28640e97f86f454776aa5df8b40c53281
Abhijeet Nalawade <100460744+abhijitnalawade0101@users.noreply.github.com>
2025-08-09 23:04:44 +0530

    Update HelloServlet.java

--- src/main/java/com/abhi/webapp/HelloServlet.java
+++ src/main/java/com/abhi/webapp/HelloServlet.java
@@ -8,7 +8,7 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("<h1>Hello from Abhijeet’s WebApp!</h1>");
+        out.println("<h1>Hello from Abhijeet’s WebApp! this is webhook</h1>");
     }
 }
 
